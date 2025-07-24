75702616281a875b3666a515d1bb3ccba0b4f66c
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2025-07-24 11:08:35 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 set -x
-FIBER_REPO=${FIBER_REPO:-"https://github.com/nervosnetwork/fiber.git"}
-FIBER_BRANCH=${FIBER_BRANCH:-"develop"}
+FIBER_REPO=${FIBER_REPO:-"https://github.com/Officeyutong/fiber.git"}
+FIBER_BRANCH=${FIBER_BRANCH:-"wasm-db-implementation"}
 git clone $FIBER_REPO
 cd fiber 
 git checkout $FIBER_BRANCH
